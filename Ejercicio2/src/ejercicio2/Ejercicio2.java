bed8193bce9c9a326a3e87124cd4349ed1b6ce7f
Christian Aqueveque <theghostcode@MacBook-Pro-de-Christian.local>
2019-10-08 19:41:05 -0300

    Se agregan comentarios ejercicios 2 y 4

--- Ejercicio2/src/ejercicio2/Ejercicio2.java
+++ Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -7,44 +7,46 @@ public class Ejercicio2 {
     
     public static void main(String[] args) {
         
+
+	//creacion del objeto Scanner llamado entrada
         Scanner entrada = new Scanner (System.in);
+	//inicializacion de variables
         int cant;
         int num;
         int cont = 1;
         int pos = 0;
         int neg = 0;       
         
-        
+        //inicio de estructura try
         try{   
-            System.out.print("Ingrese cantidad de números que desea ingresar: ");       
-            cant = entrada.nextInt();
-            while (cont <= cant){
-                System.out.print("Ingrese numero "+cont+" de "+cant+ ": ");
-                num = entrada.nextInt();
-                if(cont == 1){
-                    if(num<0){
-                        neg=num;
+            System.out.print("Ingrese cantidad de n˙meros que desea ingresar: "); //consulta cantidad de numeros a ingresar      
+            cant = entrada.nextInt(); //lee cantidad de numeros ingresados
+            while (cont <= cant){ //inicio ciclo while para el contador
+                System.out.print("Ingrese numero "+cont+" de "+cant+ ": "); //consulta el numero que desea ingresar de cuantos
+                num = entrada.nextInt(); //lee el numero ingresado 
+                if(cont == 1){ //inicio ciclo if para guardar el primer numero ingresado
+                    if(num<0){ //ciclo if para saber si el primer numero ingresado es positivo o negativo
+                        neg=num; //si es negativo
                     }
                     else{
-                        pos=num;
+                        pos=num; //si es positivo
+                    } //fin del ciclo para guardar el primer termino
+                }else{ //comienza la comparacion con el resto de los numeros
+                    if(num <= pos && num > 0){ //si el numero ingresado es menor al positivo guardado
+                        pos = num; //guardar el numero menor
                     }
-                }else{
-                    if(num <= pos && num > 0){ 
-                        pos = num;
-                    }
-                    if(num <= neg && 0 > num){
-                        neg = num;
-                    }
-                }
-                cont = cont + 1;
-            }
-            System.out.println("El número mayor negativo ingresado es "+neg);
-            System.out.println("El número menor positivo ingresado es "+pos);
-        }
-        catch(Exception e)
+                    if(num <= neg && 0 > num){ //si el numero ingresado es mayor al negativo guardado
+                        neg = num; //guardar el numero mayor                   }
+                } //fin ciclo if de guardar numero
+                cont = cont + 1; //sumar contador para la consulta de numero a ingresar
+            } //fin ciclo while
+            System.out.println("El n˙mero mayor negativo ingresado es "+neg); //muestra por pantalla el mayor negativo
+            System.out.println("El n˙mero menor positivo ingresado es "+pos); //muestra por pantalla el menor positivo
+        }//End While
+        }catch(Exception e) //valida que el dato sea un entero (int)
         {
-            System.out.println("Debe ingresar un numero entero");
+            System.out.println("Debe ingresar un numero entero"); //muestra por pantalla que el dato es incorrecto
             entrada.nextLine();
-        }    
+        }//fin estructura key   
     }
 }
--- Ejercicio4/src/ejercicio4/Ejercicio4.java
+++ Ejercicio4/src/ejercicio4/Ejercicio4.java
@@ -8,37 +8,46 @@ public class Ejercicio4 {
 
     
     public static void main(String[] args) {
+
+	//inicio objeto Scanner llamado entrada
         Scanner entrada = new Scanner(System.in);
         
+	//variables
         double a, b, c;
-        
-        System.out.print("Ingrese término a: ");
-        a = entrada.nextDouble();
-        System.out.println(a+"x^2");
-        System.out.print("Ingrese término b: ");
-        b = entrada.nextDouble();
-        System.out.println(a+"*x^2 "+b+"x");
-        System.out.print("Ingrese término c: ");
-        c = entrada.nextDouble();
-        System.out.println(a+"*x^2 "+b+"x " +c);
-        
-        double neg = (b*-1);
-        double b1 = Math.pow(b, 2);
-        double b2 = (4*a*c);
-        double d = (2*a);
-        double b3 = (b1-b2);
-        
-        if(b3>=0){            
-            double b4 = Math.pow(b3, 0.5);
-            double b5 = ((neg-b4)/d);
-            double b6 = ((neg+b4)/d);
+             
+        //inicia ciclo try
+        try
+        {        
+            System.out.print("Ingrese término a: "); //ordena a ingresar un termino 
+            a = entrada.nextDouble(); //lee el primer termino
+            System.out.println(a+"x^2"); //muestra como va quedando la ecuacion
+            System.out.print("Ingrese tÈrmino b: "); //ordena segundo numero
+            b = entrada.nextDouble(); //lee el segundo termino
+            System.out.println(a+"*x^2 "+b+"x"); //muestra como va quedando la ecuacion
+            System.out.print("Ingrese tÈrmino c: ");//ordena tercer numero
+            c = entrada.nextDouble(); //lee el tercer termino
+            System.out.println(a+"*x^2 "+b+"x " +c); //muestra como va quedando la ecuacion
+         
+            double neg = (b*-1); //multiplica el segundo termino por -1
+            double b1 = Math.pow(b, 2); //eleva el segundo termino al cuadrado
+            double b2 = (4*a*c); //multiplica el primer y tercer termino y un numero 4
+            double d = (2*a); //multiplica el primer termino por un 2
+            double b3 = (b1-b2); //resta el segundo resultado con el tercer resultado
+            
+            if(b3>=0){ //inicio ciclo if si es que el sexto resultado es positivo
+                double b4 = Math.pow(b3, 0.5); //saca la raiz cuadrada del sexto resultado
+                double b5 = ((neg-b4)/d); //el primer resultado menos la raiz cuadrada divido el cuarto resultado
+                double b6 = ((neg+b4)/d); //el primer resultado mas la raiz cuadrada divido el cuarto resultado
 
-            System.out.println("El resultado de su ecuacion es x=:"+b6+", y="+b5);
+                System.out.println("El resultado de su ecuacion es x=:"+b6+", y="+b5); //muestra por pantalla los resultados del if
+            }
+            else{
+                System.out.println("El resultado no se encuentra entre los reales"); //muestra por pantalla si es que el sexto resultado da negativo
+            }
         }
-        else{
-            System.out.println("El resultado no se encuentra entre los reales");
-        }
-        
-    }
-    
+        catch(Exception e) //valida en que el dato ingresado sea un numero
+        {
+            System.out.println("Debe ingresar un n˙mero..."); //muestra por pantalla si el dato ingresado es incorrecto
+        } //fin ciclo try
+    } 
 }
